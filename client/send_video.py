e186594ae98c48baa74f977f879e7aeef240f51e
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-02-03 20:00:19 +0100

    Add argparse to client to make it more usable without rewrites.

--- client/send_video.py
+++ client/send_video.py
@@ -10,6 +10,8 @@ from artnet import ArtnetProvider
 from util import grouper, flatmap, bilinear
 from datetime import datetime
 
+import argparse
+
 
 def get_white_noise_image(width, height):
     pil_map = Image.new("RGB", (width, height), 255)
@@ -30,83 +32,109 @@ def pixel_at(image: Image, x: float, y: float):
     r = bilinear(image, x * (image.width - 1), y * (image.height - 1))
     return r
 
-print("Getting Server Info")
 
-ip = "192.168.2.126"
-endpoint = "concentric"
+def run(ip: str, endpoint: str, img: Image, simulate_rotation: bool = False):
+    print("Getting Server Info")
 
-server_info = requests.get(f"http://{ip}/i/cc").json()
-#print(server_info)
+    server_info = requests.get(f"http://{ip}/i/cc").json()
+    #print(server_info)
 
-artnet_info = server_info[endpoint]
-port = int(artnet_info["port"])
+    artnet_info = server_info[endpoint]
+    port = int(artnet_info["port"])
 
-if endpoint == "concentric":
-    pixels = list(grouper(2, artnet_info["pixels"]))
+    if endpoint == "concentric":
+        pixels = list(grouper(2, artnet_info["pixels"]))
 
-sock = socket.socket(
-    socket.AF_INET,    # Internet
-    socket.SOCK_DGRAM  # UDP
-)
+    sock = socket.socket(
+        socket.AF_INET,    # Internet
+        socket.SOCK_DGRAM  # UDP
+    )
 
-artnet_provider = ArtnetProvider(
-    universe=0,
-    subnet=0,
-    net=0
-)
+    artnet_provider = ArtnetProvider(
+        universe=0,
+        subnet=0,
+        net=0
+    )
 
-seconds_per_frame = 1.0 / 30.0
-simulated_rotation_seconds = 3
+    seconds_per_frame = 1.0 / 30.0
+    simulated_rotation_seconds = 3 if simulate_rotation else 0
 
-print(f"Sending Art-Net Data to: {ip}:{port}!")
-sequence_start = datetime.now()
+    print(f"Sending Art-Net Data to: {ip}:{port}!")
+    sequence_start = datetime.now()
 
-simulated_rotation = 0
+    simulated_rotation = 0
 
-try:
-    while True:
-        frame_start = datetime.now()
+    try:
+        while True:
+            frame_start = datetime.now()
 
-        #img = get_white_noise_image(64, 64)
-        img = get_image_file("two_color_square.png")
+            #img = get_white_noise_image(64, 64)
+            img = get_image_file("two_color_square.png")
 
-        if endpoint == "concentric":
-            data = bytes(flatmap(
-                lambda t: pixel_at(img, *t),
-                pixels
-            ))
-        else:
-            img = img.resize((64, 64))
-            data = img.tobytes("raw")
+            if endpoint == "concentric":
+                data = bytes(flatmap(
+                    lambda t: pixel_at(img, *t),
+                    pixels
+                ))
+            else:
+                img = img.resize((64, 64))
+                data = img.tobytes("raw")
 
-        # data = io.BytesIO()
-        # img.save(data, format='JPEG', progression=False)
-        # print(data.tell())
-        # data.seek(0, 0)
-        packets = artnet_provider(data)
-        for packet in packets:
-            sock.sendto(
-                bytes(packet),
-                (ip, port)
-            )
+            # data = io.BytesIO()
+            # img.save(data, format='JPEG', progression=False)
+            # print(data.tell())
+            # data.seek(0, 0)
+            packets = artnet_provider(data)
+            for packet in packets:
+                sock.sendto(
+                    bytes(packet),
+                    (ip, port)
+                )
 
-        if artnet_provider.sequence == 0:
-            print(f"Sequence Pushed! RGB Pixels: {len(data) / 3}, Packets p.f.: {len(packets)}, FPS: {255.0 / (frame_start - sequence_start).total_seconds()}")
-            sequence_start = frame_start
+            if artnet_provider.sequence == 0:
+                print(f"Sequence Pushed! RGB Pixels: {len(data) / 3}, Packets p.f.: {len(packets)}, FPS: {255.0 / (frame_start - sequence_start).total_seconds()}")
+                sequence_start = frame_start
 
+            if simulated_rotation_seconds > 0:
+                requests.post(f"http://{ip}/rotation/set", data={"rotation": simulated_rotation})
+                simulated_rotation = (simulated_rotation + seconds_per_frame / simulated_rotation_seconds) % 1.0
+
+            # r = requests.post(
+            #     f"http://{url}/i/cc/rgb",
+            #     data=data
+            # )
+            # print(r)
+
+            time_this_frame = datetime.now() - frame_start
+            if time_this_frame.total_seconds() < seconds_per_frame:
+                time.sleep(seconds_per_frame - time_this_frame.total_seconds())
+    finally:
         if simulated_rotation_seconds > 0:
-            requests.post(f"http://{ip}/rotation/set", data={"rotation": simulated_rotation})
-            simulated_rotation = (simulated_rotation + seconds_per_frame / simulated_rotation_seconds) % 1.0
+            requests.post(f"http://{ip}/rotation/set", data={"rotation": "-1"})
 
-        # r = requests.post(
-        #     f"http://{url}/i/cc/rgb",
-        #     data=data
-        # )
-        # print(r)
 
-        time_this_frame = datetime.now() - frame_start
-        if time_this_frame.total_seconds() < seconds_per_frame:
-            time.sleep(seconds_per_frame - time_this_frame.total_seconds())
-finally:
-    if simulated_rotation_seconds > 0:
-        requests.post(f"http://{ip}/rotation/set", data={"rotation": "-1"})
+command_parser = argparse.ArgumentParser()
+
+command_parser.add_argument(
+    "ip", default="192.168.1.4",
+)
+command_parser.add_argument(
+    "--endpoint", "-e",
+    default="concentric"
+)
+command_parser.add_argument(
+    "--image", "-i",
+    default="two_color_square.png"
+)
+command_parser.add_argument("--simulate-rotation", action="store_true")
+
+
+if __name__ == "__main__":
+    args = command_parser.parse_args()
+
+    run(
+        ip=args.ip,
+        endpoint=args.endpoint,
+        img=args.image,
+        simulate_rotation=args.simulate_rotation
+    )
