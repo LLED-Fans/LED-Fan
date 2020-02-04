3e9daa8ad2a5cdd3c78e3dc47e7f77a154123521
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-02-04 18:14:38 +0100

    Add a crude python build calculator.

---
+++ calculate_build.py
@@ -0,0 +1,56 @@
+import argparse
+
+AMPERE_PER_LED = 0.06
+LED_VOLTAGE = 5
+CLOCK_HZ = 2000
+
+command_parser = argparse.ArgumentParser()
+
+command_parser.add_argument(
+    "--blades",
+    required=True
+)
+command_parser.add_argument(
+    "--leds-per-blade",
+    required=True
+)
+command_parser.add_argument(
+    "--rpm",
+    required=True
+)
+command_parser.add_argument(
+    "--radius", help="in cm",
+    required=True
+)
+command_parser.add_argument(
+    "--mirror", action="store_true"
+)
+
+if __name__ == "__main__":
+    args = command_parser.parse_args()
+
+    blades = int(args.blades)
+    leds_per_blade = int(args.leds_per_blade)
+    mirror = bool(args.mirror)
+    radius = int(args.radius)
+    rpm = float(args.rpm)
+
+    total_leds = blades * leds_per_blade
+    total_ampere = AMPERE_PER_LED * total_leds
+
+    print(f"Total LEDs: {total_leds}")
+    print(f"Total Power Needed: {total_ampere * LED_VOLTAGE}W = {total_ampere}A x {LED_VOLTAGE}v")
+
+    print()
+
+    rings = total_leds * (0.5 if mirror else 1)
+    print(f"Rings: {rings}")
+    print(f"Ring Distance: {radius / rings}cm")
+
+    print(f"Pixel Passes: {rpm / 60.0 * (2 if mirror else 1)} / s")
+
+    print()
+
+    outside_speed = 2 * 3.141 * radius / 100 * (rpm / 60)
+    print(f"Outside Speed: {outside_speed} m/s")
+    print(f"Outside Motion: {outside_speed / CLOCK_HZ * 100} cm/tick")
