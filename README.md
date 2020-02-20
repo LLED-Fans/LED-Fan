2168eb6e90f85d66e6011fbe102e466bea3dc767
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-02-20 01:15:20 +0100

    Add a bunch of comments to Setup.h. Create decode_stacktrace.sh to help with debugging stacktraces. Modify README.

--- README.md
+++ README.md
@@ -7,13 +7,15 @@ Clion Recommended:
 Relies on the flash file system for storage / static html files.
 Run uploadfs as well as upload to flash the ESP32.
 
+Take a look at Setup.h to see if it fits your build.
+
 ## Debug
 
-To debug a message, run
+(macOS only) To debug a message, run
 
-    java -jar EspStackTraceDecoder.jar ~/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-addr2line .pio/build/esp32dev/firmware.elf dump.txt
+    ./decode_stacktrace.sh <STACKTRACE>
 
-whereas dump.txt contains the stack trace.
+which creates and deletes a temporary __dmp.txt file.
 
 ## Update
 
--- src/Setup.h
+++ src/Setup.h
@@ -5,24 +5,30 @@
 #ifndef LED_FAN_SETUP_H
 #define LED_FAN_SETUP_H
 
-#define MAGNET_PINS 33
-
 #define HOST_NETWORK_SSID "LLED Fan"
 #define HOST_NETWORK_PASSWORD "We love LED"
 
+// Upper cap; depending on chipset and performance this might be lower
 #define MAX_FRAMES_PER_SECOND 2000
 
+// See https://github.com/FastLED/FastLED/blob/master/chipsets.h
 #define LED_TYPE WS2813
 #define COLOR_ORDER GRB
 #define LED_PIN 25
 #define LED_COUNT 18
 
+// Size of the square artnet-accessible screen
 #define VIRTUAL_SCREEN_SIZE 64
+
+// RingResolution = add * x + min
 #define CONCENTRIC_RESOLUTION_MIN 4
 #define CONCENTRIC_RESOLUTION_ADD 4
 
 // See ROTATION_SENSOR_TYPE_XXX
 #define ROTATION_SENSOR_TYPE ROTATION_SENSOR_TYPE_HALL_SYNC
+// Comma-Separated list of pins in use
+#define MAGNET_PINS 33
+// If applicable, max framerate of rotation sensor task
 #define ROTATION_SENSOR_MS 1
 
 #endif //LED_FAN_SETUP_H
