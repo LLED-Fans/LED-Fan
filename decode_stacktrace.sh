feb14be0070f790d6b9d3b27babee1209899ab8b
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-02-22 17:06:00 +0100

    Fix decode_stracktrace not propagating all arguments.

--- decode_stacktrace.sh
+++ decode_stacktrace.sh
@@ -1,3 +1,3 @@
-echo "$1" > __dmp.txt
+echo "$@" > __dmp.txt
 java -jar /Applications/EspStackTraceDecoder.jar ~/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-addr2line .pio/build/esp32dev/firmware.elf __dmp.txt
 rm __dmp.txt
