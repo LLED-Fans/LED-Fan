0a73871c48433dc253d455e029322c73a7a43cdb
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-05-03 20:45:22 +0200

    Output if behavior / set is != 200.

--- client/commands.py
+++ client/commands.py
@@ -8,11 +8,20 @@ def post(command: ArgumentParser, name: str, help: str, uri: Callable[[Any], str
     command.add_argument(
         name, help=help
     )
-    command.set_defaults(func=lambda args: requests.post(f"http://{args.ip}/{uri(args)}"))
+
+    def run(args):
+        res = requests.post(f"http://{args.ip}/{uri(args)}")
+        if res.status_code != 200:
+            print(res)
+            exit(res.status_code)
+
+        exit(0)
+
+    command.set_defaults(func=run)
 
 
 def setup_set_behavior(command: ArgumentParser):
     post(
         command, "id", "The behavior ID",
-        lambda args: f"/behavior/set?id={args.id}"
+        lambda args: f"behavior/set?id={args.id}"
     )
