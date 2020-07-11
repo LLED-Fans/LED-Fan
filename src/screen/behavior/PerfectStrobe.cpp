b657fed66d18d23e5c43053d895ea081d6d302ce
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-07-11 14:04:29 +0200

    Make natural strobe faster.

--- src/screen/behavior/PerfectStrobe.cpp
+++ src/screen/behavior/PerfectStrobe.cpp
@@ -12,7 +12,7 @@ PerfectStrobe::PerfectStrobe(const PRGB &color) : color(color) {}
 NativeBehavior::Status PerfectStrobe::update(Screen *screen, unsigned long delay) {
     if (!screen->rotationSensor->isReliable()) {
         if (timeUntilSwitch < delay) {
-            timeUntilSwitch = 200 * 1000;
+            timeUntilSwitch = 100 * 1000;
             isOn = !isOn;
         }
         else
